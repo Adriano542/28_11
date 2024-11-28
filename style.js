a719cb38a1a189ac3b4548b52de1360a46076d87
Adriano <122603292+Adriano542@users.noreply.github.com>
2024-11-28 11:23:43 +0100

    g

---
+++ style.js
@@ -0,0 +1,11 @@
+const imgs = Array.from(document.Images)
+const imgs1 = [...document.Images]
+document.querySelector("li:nth-child(3");
+document.getElementById('first')
+const firstElement = document.getElementById('first')
+
+
+// gdybym chciał w JS za pomocą querySelector() pobrać to co mam w nawiasach
+
+
+// WNIOSEK: inaczej pracuję kiedy mam jeden element pobrany - tak jak powyżej
